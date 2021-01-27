ce8e5b011322d4dce3264b467b874c16c0d8eee6
Marcilio <marciliorsilva93@gmail.com>
2021-01-27 15:50:38 -0300

    criando função GetProductsById

--- GetProducts/index.js
+++ GetProducts/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {
     const Products = MongoClient.collection('products');
     const res = await Products.find({});
     const body = await res.toArray();
-
+    closeConnectionFn();
     context.res = {
         status: 200,
         body
